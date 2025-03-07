e00f9cd65cd0f6b80e9699fc630b121e4f6be7d8
avgtrash <codysoloveoff@gmail.com>
2025-03-06 18:11:39 -0800

    Added admin functionality for the login screen for easy access for more development. Updated activity manifest.

--- app/src/main/java/com/lab/catclicker/MainActivity.java
+++ app/src/main/java/com/lab/catclicker/MainActivity.java
@@ -1,6 +1,10 @@
 package com.lab.catclicker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText enterName;
+    EditText enterPass;
 
+    Button login;
+    Button createAcct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        enterName = findViewById(R.id.editTextText);
+        enterPass = findViewById(R.id.editTextText2);
+
+        login = findViewById(R.id.button);
+        createAcct = findViewById(R.id.button2);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(enterName.getText().toString().toUpperCase().equals("ADMIN") && enterPass.getText().toString().toUpperCase().equals("ADMIN"))
+                {
+                    Intent i = new Intent(getApplicationContext(), ClickerActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
     }
 }
