361082cb462bcc9f3f1716063cf2f7764962e05b
avgtrash <codysoloveoff@gmail.com>
2025-03-18 18:12:53 -0700

    Added XML for create account screen. Initialized variabled in CreateAcct.java.

--- app/src/main/java/com/lab/catclicker/CreateAcct.java
+++ app/src/main/java/com/lab/catclicker/CreateAcct.java
@@ -1,6 +1,9 @@
 package com.lab.catclicker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CreateAcct extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,18 @@ public class CreateAcct extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        username = findViewById(R.id.editTextText3);
+        password = findViewById(R.id.editTextText4);
+
+        submit = findViewById(R.id.button3);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
