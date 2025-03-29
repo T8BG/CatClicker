7bde5b4491db331982bef182be0f50df2231b78f
Oleksandr Bliumenfeld <167854302+Hehich1@users.noreply.github.com>
2025-03-29 14:03:43 -0700

    small changes

--- app/src/main/java/com/lab/catclicker/ShopFragment.java
+++ app/src/main/java/com/lab/catclicker/ShopFragment.java
@@ -36,8 +36,8 @@ public class ShopFragment extends Fragment {
         recyclerView.setAdapter(adapter);
     }
     private void setUpgrades(){
-        upgrades.add((new Upgrades("Multi Cat","add a x1.5 multiplicator","20 points",CatPics[0],20)));
-        upgrades.add((new Upgrades("Funny cat","helps with clicking the cat, +5 points per click","50 points",CatPics[1],50)));
-        upgrades.add((new Upgrades("Silly Cat","Autoclicks for you, bleh","100 points",CatPics[2],100)));
+        upgrades.add((new Upgrades("Multi Cat","add a x1.5 multiplicator",CatPics[0],20)));
+        upgrades.add((new Upgrades("Funny cat","helps with clicking the cat, +5 points per click",CatPics[1],50)));
+        upgrades.add((new Upgrades("Silly Cat","Autoclicks for you, bleh",CatPics[2],100)));
     }
 }
